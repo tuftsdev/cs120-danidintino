8769ebedfbe9f764abebebee388fcb16729ae3a0
Danielle D'Intino <danielledintino7@gmail.com>
2022-10-30 15:11:58 -0400

    add nearby business and vehicle infowindows

--- notuber/script.js
+++ notuber/script.js
@@ -1,7 +1,8 @@
 var script = document.createElement("script");
 script.src =
-  "https://maps.googleapis.com/maps/api/js?key=AIzaSyAzn1PevLXcR81ojiu1QLL1JEflG4z3bxc&callback=initMap";
+  "https://maps.googleapis.com/maps/api/js?key=AIzaSyAzn1PevLXcR81ojiu1QLL1JEflG4z3bxc&callback=initMap&libraries=places";
 script.async = !0;
+
 let map;
 
 function initMap() {
@@ -15,11 +16,58 @@ function initMap() {
   }
 }
 
+function getNearbyBusinesses(myposition, type) {
+  var request = {
+    location: { lat:  myposition.coords.latitude, lng: myposition.coords.longitude },
+    radius: 1609.34, // 1 mi in meters
+    types: [type]
+  }
+
+  var service = new google.maps.places.PlacesService(map)
+  
+  service.nearbySearch(request, callback)
+
+  function callback(results, status) {
+    if(status == google.maps.places.PlacesServiceStatus.OK) {
+      results.forEach((result) => {
+        var placeLoc = result.geometry.location;
+
+        var marker = new google.maps.Marker({
+          map: map,
+          position: placeLoc
+        })
+
+        const infoWindowContent = 
+        "<p>Name: " + result.name + "</p>" +
+        "<p>Location: " + result.vicinity + "</p>" 
+      
+        const infowindow = new google.maps.InfoWindow({
+          content: infoWindowContent,
+          ariaLabel: "Business",
+        });
+      
+        marker.addListener("click", () => {
+          infowindow.open({
+            anchor: marker,
+            map,
+          });
+        });
+      })
+    }
+  }
+}
+
+
 function createCurrentPositionMaker(myposition) {
   const myMarker = new google.maps.Marker({
     position: { lat:  myposition.coords.latitude, lng: myposition.coords.longitude },
     map: map,
   });
+
+  getNearbyBusinesses(myposition, 'restaurant')
+  getNearbyBusinesses(myposition, 'bar')
+  getNearbyBusinesses(myposition, 'cafe')
+  
   
   getVehicleLocations(myposition, myMarker)
 }
@@ -47,7 +95,53 @@ function haversine_distance_in_miles(lat1, lon1, lat2, lon2) {
   return d;
 }
 
-function getVehicleLocations(position, myMarker) {
+function createInfoWindowForNearestVehicle(myPosition, myMarker, minCar, minDistance) {
+  const infoWindowContent = 
+  "<p>The closest vehicle to your position is ID " 
+  + minCar.id + " at (" + minCar.lat + ", " + minCar.lng 
+  + ") with a distance of " + minDistance.toFixed(2) + "mi.</p>"
+
+  const infowindow = new google.maps.InfoWindow({
+    content: infoWindowContent,
+    ariaLabel: "Closest vehicle",
+  });
+
+  myMarker.addListener("click", () => {
+    infowindow.open({
+      anchor: myMarker,
+      map,
+    });
+  });
+
+  new google.maps.Polyline(
+    {
+      path: [{lat: myPosition.coords.latitude, lng: myPosition.coords.longitude }, minCar], 
+      map: map
+    }
+  );
+}
+
+function createVehicleMarkerInfoWindow(carMarker, car, myPosition) {
+  const distance = haversine_distance_in_miles(myPosition.coords.latitude, myPosition.coords.longitude, car.lat, car.lng)
+  const infoWindowContent = 
+  "<p>The distance from this vehicle " 
+  + car.id + " at (" + car.lat + ", " + car.lng 
+  + ") to your location is " + distance.toFixed(2) + "mi.</p>"
+
+  const infowindow = new google.maps.InfoWindow({
+    content: infoWindowContent,
+    ariaLabel: "Vehicle " + car.id,
+  });
+
+  carMarker.addListener("click", () => {
+    infowindow.open({
+      anchor: carMarker,
+      map,
+    });
+  });
+}
+
+function getVehicleLocations(myPosition, myMarker) {
   request = new XMLHttpRequest() 
   request.open("POST", "https://jordan-marsh.herokuapp.com/rides", true) 
   request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
@@ -60,46 +154,27 @@ function getVehicleLocations(position, myMarker) {
       closeCars = JSON.parse(request.responseText)
       
       closeCars.forEach((car) => {
-        new google.maps.Marker({
+        const carMarker = new google.maps.Marker({
           position: { lat: car.lat, lng: car.lng },
           icon: "car.png",
           map: map,
           markerID: car.id,
         })
+
+        createVehicleMarkerInfoWindow(carMarker, car, myPosition)
         
-        const distance = haversine_distance_in_miles(position.coords.latitude, position.coords.longitude, car.lat, car.lng)
+        const distance = haversine_distance_in_miles(myPosition.coords.latitude, myPosition.coords.longitude, car.lat, car.lng)
         if (distance <= minDistance) {
           minDistance = distance
           minCar = car
         }
       })
 
-      const infoWindowContent = 
-      "<p>The closest vehicle to your position is ID " 
-      + minCar.id + " at (" + minCar.lat + ", " + minCar.lng 
-      + ") with a distance of " + minDistance.toFixed(2) + "mi.</p>"
-    
-      const infowindow = new google.maps.InfoWindow({
-        content: infoWindowContent,
-        ariaLabel: "Closest vehicle",
-      });
-
-      myMarker.addListener("click", () => {
-        infowindow.open({
-          anchor: myMarker,
-          map,
-        });
-      });
-
-      new google.maps.Polyline(
-        {
-          path: [{lat: position.coords.latitude, lng: position.coords.longitude }, minCar], 
-          map: map
-        }
-      );
+      createInfoWindowForNearestVehicle(myPosition, myMarker, minCar, minDistance)
     }
 };
-  var params = "username=8SeyrexS&lat=" + position.coords.latitude + "&lng=" + position.coords.longitude
+  var params = "username=8SeyrexS&lat=" + myPosition.coords.latitude + "&lng=" + myPosition.coords.longitude
   request.send(params)
 }
-document.head.appendChild(script), (window.initMap = initMap);
+document.head.appendChild(script)
+window.initMap = initMap
